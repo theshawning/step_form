0607a46fc540ea9aba8aaf665b2cb21545aa571d
Shawn Harrison <38034484+theshawning@users.noreply.github.com>
2020-04-26 13:35:58 +0000

    created UserForm component, setup step and change methods as well as Switch

--- src/components/UserForm.jsx
+++ src/components/UserForm.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import FormUserDetails from './FormUserDetails';
 
 export class UserForm extends Component {
   state = {
@@ -11,12 +12,52 @@ export class UserForm extends Component {
     bio: ''
   }
 
+  // proceed to next step
+  nextStep = () => {
+    const { step } = this.state;
+    this.setState({
+      step: step + 1
+    })
+  }
+
+  // go back to previous step
+  prevStep = () => {
+    const { step } = this.state;
+    this.setState({
+      step: step - 1
+    })
+  }
+
+  // handle fields change
+  handleChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
   render() {
-    return (
-      <div>
-        
-      </div>
-    )
+    const { step } = this.state;
+    const { firstName, lastName, email, occupation, city, bio } = this.state;
+    const values = { firstName, lastName, email, occupation, city, bio };
+
+    switch(step) {
+      case 1:
+        return (
+          <FormUserDetails
+            nextStep={this.nextStep}
+            handleChange={this.handleChange}
+            values={values}
+          />
+        )
+      case 2:
+        return <h1>FormPersonalDetails</h1>
+      case 3:
+        return <h1>Confirm</h1>
+      case 4:
+        return <h1>Success</h1>
+      default:
+        return;
+    }
   }
 }
 
